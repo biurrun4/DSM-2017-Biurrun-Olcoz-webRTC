302516771c51c04be2882f20d4a836cf75a2c56c
Carlos Biurrun <biurrun4@outlook.com>
2017-02-23 10:37:30 +0100

    Añadido demo de Marko y editado con credenciales del 5

---
+++ 5-simplewebrtc-signaling/demo/xirsys_connect.js
@@ -0,0 +1,37 @@
+// 'ident' and 'secret' should ideally be passed server-side for security purposes.
+// If secureTokenRetrieval is true then you should remove these two values.
+
+// Insecure method
+var xirsysConnect = {
+	secureTokenRetrieval : false,
+	data : {
+		channel : 'HallOfFame',
+		ident : 'biurrun4',
+		secret : 'e3986b3c-f9a9-11e6-8c09-0c87b6d55741'
+	}
+};
+/*
+var xirsysConnect = {
+	secureTokenRetrieval : false,
+	data : {
+		domain : 'dsm2017',
+		application : 'default',
+		room : 'default',
+		ident : 'markogalarza',
+		secret : '8ef83a62-eb17-11e6-bee9-09d9990f7fd5',
+		secure : 0
+	}
+};*/
+
+// Secure method
+/*var xirsysConnect = {
+	secureTokenRetrieval : true,
+	server : '../getToken.php',
+	data : {
+		domain : '< www.yourdomain.com >',
+		application : 'default',
+		room : 'default',
+		secure : 1
+	}
+};*/
+
